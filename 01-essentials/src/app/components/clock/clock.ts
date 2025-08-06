b67c51e89ce0fcec7ab7a95cd7aac6de888efba5
La Mela, Jonathan <jlamela@bethel.jw.org>
2025-08-06 20:23:36 +0200

    added tailwind and daisy to chap01

--- 01-essentials/src/app/components/clock/clock.ts
+++ 01-essentials/src/app/components/clock/clock.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, computed, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-clock',
@@ -7,12 +7,25 @@ import { Component, OnInit, signal } from '@angular/core';
   styleUrl: './clock.css'
 })
 export class Clock implements OnInit {
-  protected readonly title = signal('01-essentials');
+
+  currentTime = signal(new Date());
+  currentHour = computed(() => {
+    const hours = this.currentTime().getHours();
+    return hours < 10 ? `0${hours}` : hours.toString();
+  });
+  currentMinute = computed(() => {
+    const minutes = this.currentTime().getMinutes();
+    return minutes < 10 ? `0${minutes}` : minutes.toString();
+  });
+  currentSecond = computed(() => {
+    const seconds = this.currentTime().getSeconds();
+    return seconds < 10 ? `0${seconds}` : seconds.toString();
+  });
 
   ngOnInit() {
     // Initialization logic here
     setInterval(() => {
-      this.title.set(new Date().toLocaleTimeString());
+      this.currentTime.set(new Date());
     }, 1000);
   }
 }

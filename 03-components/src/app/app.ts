52a39b7be4947139afc9540147eece0f8c95b8d1
La Mela, Jonathan <jlamela@bethel.jw.org>
2025-08-07 16:06:22 +0200

    chap03 updates

--- 03-components/src/app/app.ts
+++ 03-components/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MyButton } from "./my-button/my-button";
 import { MyInput } from "./my-input/my-input";
+import { Container } from "./container/container";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MyButton, MyInput],
+  imports: [RouterOutlet, MyButton, MyInput, Container],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

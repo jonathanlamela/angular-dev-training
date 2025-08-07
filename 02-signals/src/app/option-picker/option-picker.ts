053cd3c0ea209721bc9f520244db1bd3a4835dc4
La Mela, Jonathan <jlamela@bethel.jw.org>
2025-08-07 11:51:19 +0200

    use of linkedSignal

--- 02-signals/src/app/option-picker/option-picker.ts
+++ 02-signals/src/app/option-picker/option-picker.ts
@@ -15,7 +15,13 @@ export class OptionPicker {
     { id: 'SR', name: 'Siracusa' },
     { id: 'TP', name: 'Trapani' }
   ]);
-  selectedOption = linkedSignal(() => this.optionsAvailable()[0]);
+
+  selectedOption = linkedSignal<OptionValue[], OptionValue>({
+    source: this.optionsAvailable,
+    computation: (options, previous) => {
+      return options.find((option) => option.id === previous?.value.id) || options[0]
+    }
+  });
 
   pickCity(index: string) {
     this.selectedOption.set(this.optionsAvailable().find(city => city.id === index)!);
@@ -23,6 +29,17 @@ export class OptionPicker {
 
   deleteCity(index: string) {
     this.optionsAvailable.update(options => options.filter(city => city.id !== index));
+  }
+
+  shuffleOptions() {
+    this.optionsAvailable.update(options => {
+      const shuffled = [...options];
+      for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+      }
+      return shuffled;
+    });
   }
 
 }
